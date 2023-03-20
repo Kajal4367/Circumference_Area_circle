1a979ef56004ea45d111cf5b65bbe956e3c13e27
Kajal Kumari <96781807+Kajal4367@users.noreply.github.com>
2023-03-20 14:36:58 +0530

    Create Circumference_area.py

---
+++ Circumference_area.py
@@ -0,0 +1,7 @@
+# Circumference and area of a circle with given radius
+PI = 3.14
+radius = float(input("Enter radius of circle:"))
+circumference = 2*PI*radius
+Area = PI*radius*radius
+print("\n Circumference of the circle is", circumference)
+print("\n Area of circle is", Area)
